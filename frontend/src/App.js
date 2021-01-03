c25385792d635d57f4755ea5d87486d9e5c56ced
Reine0017 <m.fortitudo.fr@gmail.com>
2021-01-03 23:30:12 +0900

    development works

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,14 @@ function App() {
   const [getMessage, setGetMessage] = useState({})
 
   useEffect(()=>{
-    axios.get('https://react-flask-158.herokuapp.com/flask/hello').then(response => {
+    // axios.get('https://react-flask-158.herokuapp.com/flask/hello').then(response => {
+    //   console.log("SUCCESS", response)
+    //   setGetMessage(response)
+    // }).catch(error => {
+    //   console.log(error)
+    // })
+
+    axios.get('http://localhost:5000/flask/hello').then(response => {
       console.log("SUCCESS", response)
       setGetMessage(response)
     }).catch(error => {
@@ -20,10 +27,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>React + Flask Tutorial</p>
-        <h3>{getMessage.status === 200 ? 
+        <div>{getMessage.status === 200 ? 
           <h3>{getMessage.data.message}</h3>
           :
-          <h3>LOADING</h3>}</h3>
+          <h3>LOADING</h3>}</div>
       </header>
     </div>
   );
