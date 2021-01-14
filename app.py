c12891339c7e6e7f87750504056c9efe2159d689
Reine0017 <m.fortitudo.fr@gmail.com>
2021-01-14 14:14:16 +0900

    changes

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, send_from_directory
 from flask_restful import Api, Resource, reqparse
 from flask_cors import CORS #comment this
-from core.HelloApiHandler import HelloApiHandler
+from api.HelloApiHandler import HelloApiHandler
 
 
 app = Flask(__name__, static_url_path='', static_folder='frontend/build')
